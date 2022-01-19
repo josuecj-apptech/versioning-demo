ecf301392eca57945849a7353ec4378a76a98180
josuecj-apptech <josue.cj@appeiron.tech>
2022-01-19 11:26:08 -0500

    Different versioning implemented

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Version, VERSION_NEUTRAL } from '@nestjs/common';
+import { ApiHeader } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller({ path: 'hello' })
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Version('1')
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // Uncomment if want to use HEADER versioning in swagger
+  // @ApiHeader({
+  //   name: 'Version-header',
+  //   enum: ['1', '2'],
+  //   description: 'Select a version',
+  //   required: true,
+  // })
+  @Version('2')
+  @Get()
+  getHello2(): string {
+    return this.appService.getHello2();
+  }
+
+  @Version(VERSION_NEUTRAL)
+  @Get('neutral')
+  getExample(): string {
+    return 'This method is independent from a version';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    // URI VERSIONING CONFIG (http://localhost:3000/v2/hello)
+    type: VersioningType.URI,
+
+    // HEADER-TYPE VERSIONING CONFIG (TO USE WITH POSTMAN ADD HEADER 'Version-Header' AND GIVE IT A VALUE)
+    // type: VersioningType.HEADER,
+    // header: 'Version-Header',
+
+    // MEDIA-TYPE VERSIONING CONFIG (TO USE WITH POSTMAN CHANGE VALUE OF HEADER 'Accept' AND GIVE IT VALUE application/json;v=1)
+    // type: VersioningType.MEDIA_TYPE,
+    // key: 'v=',
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Versioning example')
